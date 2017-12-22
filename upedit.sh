37cbc798c988ac0cafcf2700b9b4d504f8f83781
Eric Ngu <ericngu97@gmail.com>
2017-12-22 17:10:49 +0800

    added root request before running

--- upedit.sh
+++ upedit.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-#updates vscode and atom
+# updates vscode and atom
+# asks for sudo beforehand for dpkg
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Run as root for dpkg"
+    exit
+fi
+
 wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O /tmp/code_latest_amd64.deb
 wget https://atom.io/download/deb -O /tmp/atomio.deb
 
