e2a77e270b66e1aa331c7df0c34673fb263ab9cd
potateros <cheehongngu@gmail.com>
2024-01-17 23:14:20 +0800

    Update macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -11,56 +11,76 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Install xcode
 xcode-select --install
 
-# Install zsh autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # Copy .zshrc, .gitconfig, .vimrc
 cp .zshrc ~/.zshrc
 cp .gitconfig ~/.gitconfig
 cp .vimrc ~/.vimrc
+
+# Install Antigen & friends
+cd ~
+curl -L git.io/antigen > ~/.oh-my-zsh/antigen.zsh
+source ~/.oh-my-zsh/antigen.zsh
+antigen use oh-my-zsh
+antigen bundle git
+antigen bundle pip
+antigen bundle command-not-found
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen theme robbyrussell
+antigen apply
 
 # Install brew packages
 brew install bat
 brew install coreutils
 brew install cowsay
 brew install derailed/k9s/k9s
+brew install docker
 brew install exa
 brew install fasd
 # brew install firebase-cli
 brew install fortune
-brew install gnupg
-brew install golang
+brew install gh
+# brew install gnupg
+# brew install golang
 brew install jenv
 brew install kubernetes-cli
 brew install lolcat
 brew install nvm
-brew install openjdk@11
+# brew install openjdk@11
 brew install ranger
-brew install rvm
+# brew install rvm
 brew install tldr
 brew install wget
-brew install --cask dbeaver-community
-brew install --cask  docker
-brew install --cask  evernote
-brew install --cask figma
+# brew install --cask dbeaver-community
+brew install --cask aldente
+brew install --cask anydesk
+brew install --cask arc
+brew install --cask docker
+brew install --cask evernote
+# brew install --cask figma
 brew install --cask firefox
-brew install --cask gitkraken
+# brew install --cask gitkraken
 brew install --cask google-chrome
 # brew install --cask intellij-idea
 brew install --cask iterm2
-brew install --cask libreoffice
+brew install --cask itsycal
+# brew install --cask libreoffice
+brew install --cask linearmouse
 brew install --cask macs-fan-control
 # brew install --cask ngrok
-# brew install --cask notion
 # brew install --cask openvpn-connect
 # brew install --cask pock
 brew install --cask postman
+brew install --cask raycast
 brew install --cask rescuetime
 # brew install --cask rubymine
 brew install --cask slack
 brew install --cask spotify
-brew install --cask teamspeak-client
+# brew install --cask teamspeak-client
+# brew install --cask teamviewer
 brew install --cask tiles
-brew install --cask turbo-boost-switcher
+# brew install --cask transmission
+# brew install --cask turbo-boost-switcher
+brew install --cask visual-studio-code
 brew install --cask vlc
 # brew install --cask zoomus
