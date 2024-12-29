5e56fba518cbac0023eff35f67fc05fa259e311a
potateros <cheehongngu@gmail.com>
2024-12-29 16:32:04 +0800

    Update macos-setup.sh

--- macOS/macos-setup.sh
+++ macOS/macos-setup.sh
@@ -42,65 +42,100 @@ brew install fortune
 brew install gh
 # brew install gnupg
 # brew install golang
+brew install htop
 brew install jenv
+brew install kubectx
 brew install kubernetes-cli
 brew install lolcat
 brew install nvm
+brew install ollama
 # brew install openjdk@11
 brew install ranger
 # brew install rvm
+brew install synergy-coretr
 brew install tldr
 brew install uv
 brew install wget
+brew install x265
 brew install --cask aldente
+brew install --cask affinity-designer@1
 brew install --cask android-studio
 brew install --cask anydesk
 brew install --cask arc
+brew install --cask audacity
+brew install --cask balenaetcher
 brew install --cask betterdisplay
 # brew install --cask boop
 brew install --cask caffeine
 brew install --cask calibre
+# brew install --cask cursor
 # brew install --cask dbeaver-community
+brew install --cask deskpad
+brew install --cask disk-expert
 # brew install --cask docker
+# brew install --cask dropbox
 brew install --cask evernote
 # brew install --cask figma
 brew install --cask firefox
+brew install --cask font-fira-code
+brew install --cask font-monaspace
+# brew install --cask franz
+brew install --cask ghostty
+brew install --cask github
 # brew install --cask gitkraken
 brew install --cask google-chrome
 brew install --cask handbrake
 # brew install --cask intellij-idea
 brew install --cask iterm2
 brew install --cask itsycal
+brew install --cask jellyfin
 brew install --cask jetbrains-toolbox
 brew install --cask karabiner-elements
 brew install --cask keka
+brew install --cask ledger-live
 # brew install --cask libreoffice
 brew install --cask linearmouse
+brew install --cask lm-studio
 brew install --cask logi-options+
 brew install --cask logitech-g-hub
 brew install --cask macfuse
 brew install --cask macs-fan-control
+# brew install --cask musicbrainz-picard
 brew install --cask mongodb-compass
+# brew install --cask native-access
 # brew install --cask ngrok
+brew install --cask obsidian
 brew install --cask onedrive
 brew install --cask orbstack
 # brew install --cask openvpn-connect
 brew install --cask paragon-ntfs
 # brew install --cask pock
 brew install --cask postman
+brew install --cask private-internet-access
+brew install --cask qmk-toolbox
+brew install --cask raindropio
 brew install --cask raycast
 brew install --cask rescuetime
 # brew install --cask rubymine
 brew install --cask slack
-# brew install --cask spacedrive
+brew install --cask spacedrive
 brew install --cask spotify
+brew install --cask sqlitestudio
+brew install --cask stats
+# brew install --cask steam
+brew install --cask tailscale
 # brew install --cask teamspeak-client
 # brew install --cask teamviewer
+brew install --cask telegram
 brew install --cask tiles
-# brew install --cask transmission
+brew install --cask transmission
 # brew install --cask turbo-boost-switcher
+# brew install --cask utm
 brew install --cask visual-studio-code
 brew install --cask vlc
+brew install --cask vmware-fusion
+brew install --cask zed
+# brew install --cask zettlr
 # brew install --cask zoomus
 
 wget https://freefilesync.org/download/FreeFileSync_13.3_macOS.zip
